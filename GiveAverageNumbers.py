9b65205544bf97989ef83383b25825ddd02ff41b
Komeyl Kalhorinia <72478304+komyl@users.noreply.github.com>
2021-08-21 20:18:08 +0300

    Give Average Numbers

---
+++ GiveAverageNumbers.py
@@ -0,0 +1,29 @@
+sum = 0
+count = 0 
+x = []
+while True :
+    x = int(input())
+    if x == -1 :
+        break
+    sum += x
+    count += 1
+    averagex = sum / count
+       
+            
+    
+    
+    
+ 
+print(averagex)
+    
+        
+
+
+
+
+
+        
+
+
+        
+
