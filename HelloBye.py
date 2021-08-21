971166d832ae738269fd2dbbac06754aa3200adf
Komeyl Kalhorinia <72478304+komyl@users.noreply.github.com>
2021-08-21 20:16:51 +0300

    Say Hello and Say Bye

---
+++ HelloBye.py
@@ -0,0 +1,20 @@
+
+def hellobye():
+x = str(input())
+if x == "hi" or "hello" :
+        print("hello my Friends")
+  elif x == "Hello" or "Hi" :
+        print("hello my Friends")
+  elif x == "HELLO" or "HI" :
+        print("hello my Friends")
+  else :
+        print("Godbye My Friends , See You Later")
+
+
+
+
+
+  hellobye()
+    
+    
+
